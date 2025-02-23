5235357e8adb05aaac5ffbf73059a8ff923a12da
zer0 <osom8979@gmail.com>
2025-02-23 13:01:24 +0900

    Upgrade major version

--- type_serialize/__init__.py
+++ type_serialize/__init__.py
@@ -4,7 +4,7 @@ from type_serialize.deserialize import deserialize
 from type_serialize.interface import Serializable
 from type_serialize.serialize import serialize
 
-__version__ = "1.3.0"
+__version__ = "2.0.0"
 
 __all__ = (
     "__version__",
